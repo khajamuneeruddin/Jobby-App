7ff12f7a2a1fb1c1c69244c2bcc8d24d79dfde07
khajamuneeruddin <khwajaurs@gmail.com>
2024-04-27 12:18:51 +0000

    Jobby App

---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <Link to="/">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="home-website-logo"
+          />
+        </Link>
+        <ul className="nav-items">
+          <li>
+            <Link to="/" className="nav-link-lg">
+              Home
+            </Link>
+            <Link to="/" className="nav-link-sm">
+              <AiFillHome className="small-header-icons" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs" className="nav-link-lg">
+              Jobs
+            </Link>
+            <Link to="/jobs" className="nav-link-sm">
+              <BsFillBriefcaseFill className="small-header-icons" />
+            </Link>
+          </li>
+          <li className="logout-btn-list-item-small">
+            <label htmlFor="logoutButton" className="logout-label">
+              <button
+                id="logoutButton"
+                type="button"
+                className="logout-button-sm"
+                onClick={onClickLogout}
+              >
+                Logout
+                <FiLogOut className="logout-icon-sm" />
+              </button>
+            </label>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="logout-button-lg"
+          onClick={onClickLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
